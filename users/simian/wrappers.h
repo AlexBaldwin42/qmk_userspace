4d102eff07beb427a044ccee7a4b320574b3daf9
Alex Baldwin <abaldwin@weberbasin.com>
2026-03-06 12:47:04 -0700

    Format layer macros to visually match keyboard shape

--- users/simian/wrappers.h
+++ users/simian/wrappers.h
@@ -60,51 +60,51 @@
 #define HOME_ROW_MOD_LEFT_GACS(...) _HOME_ROW_MOD_LEFT_GACS(__VA_ARGS__)
 
 // ─── Layer row macros (36-key core) ────────────────────────────────
+// clang-format off
 
-#define _BASE_L1 ADJ_KCQ,    KC_W,     KC_E,   KC_R,    KC_T
-#define _BASE_R1                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
-#define _BASE_L2    KC_A,    KC_S,     KC_D,   KC_F,    KC_G
-#define _BASE_R2                KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN
-#define _BASE_L3    KC_Z,    KC_X,     KC_C,   KC_V,    KC_B
-#define _BASE_R3                KC_N,    KC_M, KC_COMM,  KC_DOT,ADJ_SLSH
-#define _BASE_TL             NUM_TAB,LW_BSPC, RAI_DEL
-#define _BASE_TR             RAI_ENT,  LW_SPC, NUM_ESC
+/*                    ╭─────────────────────╮                 ╭─────────────────────╮ */
+#define LAYOUT_LAYER_BASE                                                              \
+         ADJ_KCQ,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
+            KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,    \
+            KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M, KC_COMM,  KC_DOT,ADJ_SLSH,   \
+                           NUM_TAB, LW_BSPC, RAI_DEL,          RAI_ENT,  LW_SPC, NUM_ESC
 
-#define LAYOUT_LAYER_BASE \
-    _BASE_L1, _BASE_R1, \
-    _BASE_L2, _BASE_R2, \
-    _BASE_L3, _BASE_R3, \
-    _BASE_TL, _BASE_TR
+/*                    ╭─────────────────────╮                 ╭─────────────────────╮ */
+#define LAYOUT_LAYER_POINTER                                                           \
+         _______, _______, _______, _______, _______,          MS_BTN1, DRGSCRL, _______, _______, _______,    \
+         _______, _______, _______, _______, _______,          MS_BTN2, SNIPING_MODE, _______, _______, _______, \
+         _______, _______, _______, _______, _______,          _______, SNIPING_TOGGLE, _______, _______, _______, \
+                           _______, MS_BTN1, MS_BTN2,          _______, _______, _______
 
-#define LAYOUT_LAYER_POINTER \
-    _______, _______, _______, _______, _______,    MS_BTN1, DRGSCRL, _______, _______, _______, \
-    _______, _______, _______, _______, _______,    MS_BTN2, SNIPING_MODE, _______, _______, _______, \
-    _______, _______, _______, _______, _______,    _______, SNIPING_TOGGLE, _______, _______, _______, \
-                      _______, MS_BTN1, MS_BTN2,    _______, _______, _______
+/*                    ╭─────────────────────╮                 ╭─────────────────────╮ */
+#define LAYOUT_LAYER_LOWER                                                             \
+        G(KC_1), G(KC_2), G(KC_3), G(KC_4), G(KC_5),         G(KC_6), G(KC_7), G(KC_8), G(KC_9), G(KC_0),   \
+        KC_WBAK, KC_WFWD, KC_HOME,  KC_END,  KC_APP,         KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,   KC_F6,   \
+         CTL_F1,  ALT_F2, C(G(KC_LEFT)), C(G(KC_RIGHT)), KC_F5,   KC_F5,   KC_F7,   KC_F8,  ALT_F9, CTL_F10, \
+                           _______, _______, _______,         _______, _______, _______
 
-#define LAYOUT_LAYER_LOWER \
-    G(KC_1), G(KC_2), G(KC_3),  G(KC_4),G(KC_5),    G(KC_6),  G(KC_7),G(KC_8),  G(KC_9),G(KC_0), \
-    KC_WBAK, KC_WFWD, KC_HOME,   KC_END, KC_APP,    KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,  KC_F6, \
-    CTL_F1,ALT_F2,C(G(KC_LEFT)),C(G(KC_RIGHT)),  KC_F5,    KC_F5,   KC_F7,    KC_F8,  ALT_F9, CTL_F10, \
-                      _______, _______, _______,    _______, _______, _______
+/*                    ╭─────────────────────╮                 ╭─────────────────────╮ */
+#define LAYOUT_LAYER_RAISE                                                             \
+        KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_QUOT,  KC_GRV,   \
+        KC_QUOT,  KC_DQT, KC_LPRN, KC_RPRN, KC_PGUP,         KC_MINS, CTL_EQL, KC_LCBR, KC_RCBR, KC_BSLS,   \
+         KC_ESC, _______, KC_ESC,   KC_ENT, KC_PGDN,         KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE,   \
+                           _______, _______, _______,         _______, _______, _______
 
-#define LAYOUT_LAYER_RAISE \
-    KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,    KC_CIRC, KC_AMPR, KC_ASTR, KC_QUOT, KC_GRV, \
-    KC_QUOT,KC_DQT,   KC_LPRN, KC_RPRN, KC_PGUP,    KC_MINS, CTL_EQL, KC_LCBR, KC_RCBR,KC_BSLS, \
-    KC_ESC,_______,KC_ESC,KC_ENT, KC_PGDN,           KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE, \
-                      _______, _______, _______,    _______, _______, _______
+/*                    ╭─────────────────────╮                 ╭─────────────────────╮ */
+#define LAYOUT_LAYER_ADJUST                                                            \
+        XXXXXXX, TG(LAYER_NUMPAD), KC_CAPS, XXXXXXX, XXXXXXX,   QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, RM_TOGG, \
+        RM_TOGG, RM_NEXT, RM_VALU, RM_VALD, XXXXXXX,         XXXXXXX, KC_WBAK, KC_WFWD,  KC_APP, DRG_TOG,   \
+        RM_NEXT, RM_PREV, RM_HUEU, RM_HUED, QK_BOOT,         XXXXXXX, XXXXXXX,DPI_DOWN,  DPI_UP,DPI_CYCLE,  \
+                           _______, _______, _______,         KC_VOLD, KC_VOLU, KC_MPLY
 
-#define LAYOUT_LAYER_ADJUST \
-    XXXXXXX,TG(LAYER_NUMPAD), KC_CAPS, XXXXXXX, XXXXXXX,    QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, RM_TOGG, \
-    RM_TOGG, RM_NEXT, RM_VALU, RM_VALD, XXXXXXX,            XXXXXXX, KC_WBAK, KC_WFWD,  KC_APP, DRG_TOG, \
-    RM_NEXT,RM_PREV, RM_HUEU, RM_HUED, QK_BOOT,             XXXXXXX, XXXXXXX, DPI_DOWN, DPI_UP, DPI_CYCLE, \
-                      _______, _______, _______,    KC_VOLD, KC_VOLU, KC_MPLY
+/*                    ╭─────────────────────╮                 ╭─────────────────────╮ */
+#define LAYOUT_LAYER_NUMPAD                                                            \
+  TG(LAYER_NUMPAD),   KC_F9,   KC_F8,   KC_F7,  KC_F12,         KC_CIRC,    KC_7,    KC_8,    KC_9, KC_ASTR,  \
+           KC_NO,   KC_F6,   KC_F5,   KC_F4,  KC_F11,         KC_MINS,    KC_4,    KC_5,    KC_6,  KC_EQL,  \
+         KC_LSFT,   KC_F3,   KC_F2,   KC_F1,  KC_F10,         KC_PLUS,    KC_1,    KC_2,    KC_3, KC_SLSH,  \
+                           _______, _______, _______,          KC_ENT,    KC_0,  KC_DOT
 
-#define LAYOUT_LAYER_NUMPAD \
-    TG(LAYER_NUMPAD), KC_F9,   KC_F8,  KC_F7,  KC_F12,    KC_CIRC,   KC_7,   KC_8,   KC_9, KC_ASTR, \
-    KC_NO,  KC_F6,   KC_F5,  KC_F4,  KC_F11,              KC_MINS,   KC_4,   KC_5,   KC_6,  KC_EQL, \
-    KC_LSFT,  KC_F3,   KC_F2,  KC_F1,  KC_F10,            KC_PLUS,   KC_1,   KC_2,   KC_3, KC_SLSH, \
-                      _______, _______, _______,    KC_ENT,    KC_0, KC_DOT
+// clang-format on
 
 // ─── Wrapper macros for different keyboard layouts ─────────────────
 
